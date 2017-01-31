4edb825d6c71ab4451ac783e0c60988188b85ba2
blackmiaool <blackmiaool@gmail.com>
2017-01-31 19:24:54 +0800

    fix config

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,4 +1,4 @@
-var config = require('../config')
+var config = require('../config/index')
 var webpack = require('webpack')
 var merge = require('webpack-merge')
 var utils = require('./utils')
@@ -8,29 +8,31 @@ var FriendlyErrors = require('friendly-errors-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+    baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
 })
 
 module.exports = merge(baseWebpackConfig, {
-  module: {
-    loaders: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
-  },
-  // eval-source-map is faster for development
-  devtool: '#eval-source-map',
-  plugins: [
+    module: {
+        loaders: utils.styleLoaders({
+            sourceMap: config.dev.cssSourceMap
+        })
+    },
+    // eval-source-map is faster for development
+    devtool: '#eval-source-map',
+    plugins: [
     new webpack.DefinePlugin({
-      'process.env': config.dev.env
-    }),
+            'process.env': config.dev.env
+        }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
-    }),
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        }),
     new FriendlyErrors()
   ]
 })
