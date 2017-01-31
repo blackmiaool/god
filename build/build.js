f03ddae1072c61c24e7d5be7b1940206dfe060f9
blackmiaool <blackmiaool@gmail.com>
2017-01-31 19:32:46 +0800

    change initial model of login page

--- build/build.js
+++ build/build.js
@@ -4,15 +4,15 @@ require('shelljs/global')
 env.NODE_ENV = 'production'
 
 var path = require('path')
-var config = require('../config')
+var config = require('../config/index')
 var ora = require('ora')
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
 
 console.log(
-  '  Tip:\n' +
-  '  Built files are meant to be served over an HTTP server.\n' +
-  '  Opening index.html over file:// won\'t work.\n'
+    '  Tip:\n' +
+    '  Built files are meant to be served over an HTTP server.\n' +
+    '  Opening index.html over file:// won\'t work.\n'
 )
 
 var spinner = ora('building for production...')
@@ -24,13 +24,13 @@ mkdir('-p', assetsPath)
 cp('-R', 'static/*', assetsPath)
 
 webpack(webpackConfig, function (err, stats) {
-  spinner.stop()
-  if (err) throw err
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n')
+    spinner.stop()
+    if (err) throw err
+    process.stdout.write(stats.toString({
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false
+    }) + '\n')
 })
