019d947c1723c83982aa9e5cb459e3f18c5d199f
blackmiaool <blackmiaool@gmail.com>
2017-02-05 17:24:03 +0800

    delete config

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    serverPort: 9012,
-    domain: "blackmiaool.jios.org",
-    firstRoom: {
-        name: "god",
-        bulletin: "Common room",
-        avatar: "/static/img/avatar.gif"
-    },
-}
